3521d9f4ac1411787b5a58bc49743b10fd6ebd97
Allin RAMANAMPISOA <allin13ramanampisoa@outlook.fr>
2025-11-20 13:22:07 +0300

    task - 001 image

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,24 +12,24 @@ export const projects: Project[] = [
     title: "Plateforme SAAS",
     description: "SAAS de gestion de pointage et de ronde",
     technologies: ["Symfony", "MySQL", "APIPlatform"],
-    image: "https://via.placeholder.com/400x250/3B82F6/FFFFFF?text=SAAS+Platform",
+    image: "/saas.png",
   },
   {
     title: "Système CRM",
     description: "Solution CRM complète pour la gestion des relations clients et des ventes.",
     technologies: ["Symfony", "MySQL", "APIPlatform"],
-    image: "https://via.placeholder.com/400x250/10B981/FFFFFF?text=CRM+System",
+    image: "/crm.png",
   },
   {
     title: "Freelance Dispo",
     description: "Mise en relation entre Freelance et entreprise dans le domaine du tech",
     technologies: ["Symfony", "MySQL", "Stripe"],
-    image: "https://via.placeholder.com/400x250/F59E0B/FFFFFF?text=Freelance+Platform",
+    image: "/freelance-dispo.png",
   },
   {
     title: "Intégration IA",
     description: "Mise en place de fonctionnalités basées sur l'IA, incluant l'analyse de données",
     technologies: ["Neuron AI", "OpenAI", "MCP"],
-    image: "https://via.placeholder.com/400x250/8B5CF6/FFFFFF?text=AI+Integration",
+    image: "/ia.png",
   },
 ];
